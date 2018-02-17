2dad13a52897175aed92237ae8e46bdba316d549
Muhammad Avied Bachmid <aviedbachmid.ab@gmail.com>
2018-02-17 21:41:32 +0700

    Update client/index.js and components/LyricCreate.js for automatic refetch lyrics

--- client/index.js
+++ client/index.js
@@ -11,7 +11,9 @@ import SongList from './components/songList'
 import SongCreate from './components/SongCreate'
 import SongDetail from './components/SongDetail'
 
-const client = new ApolloClient({})
+const client = new ApolloClient({
+  dataIdFromObject: o => o.id
+})
 
 const Root = () => {
   return (
